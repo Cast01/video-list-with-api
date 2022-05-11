754dabaab123c0d0f92554b7544e151483ae7ad7
Cast01 <gabriel_floriano_castilho@outlook.com>
2022-05-11 19:52:09 -0300

    VideoTeca-Huriel(Youtube)

--- video-list-api/src/database.js
+++ video-list-api/src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectToDataBase() {
-  mongoose.connect(process.env.DATABASE_URL);
+  mongoose.connect(process.env.MONGODB_URI);
   const db = mongoose.connection;
   db.on("error", (err) => console.error(err));
   db.once("open", () => console.log("Conectado com o MongoDB"));
